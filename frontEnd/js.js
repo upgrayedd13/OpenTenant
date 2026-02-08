aa3d93b9f5f40982dc99f37909d744e878dfb8bf
merahn <merahn@iu.edu>
2026-02-08 15:54:37 -0500

    basic login page

--- frontEnd/js.js
+++ frontEnd/js.js
@@ -1,8 +1,16 @@
-const menuLinks = document.querySelectorAll(".sidebar a");
+const sidebar = document.querySelector(".sidebar");
+    const menuLinks = document.querySelectorAll(".sidebar a");
 
     menuLinks.forEach(link => {
         link.addEventListener("click", () => {
+
+            // remove active from all links
             menuLinks.forEach(l => l.classList.remove("active"));
+
+            // add active to clicked one
             link.classList.add("active");
+
+            // move clicked link to the top
+            sidebar.prepend(link);
         });
     });
