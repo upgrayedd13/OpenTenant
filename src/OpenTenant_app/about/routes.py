b6c19c8cc164f19b694bee8c12d2b934f8394d0e
Heidi Ganac Nicholson <heidiganac@gmail.com>
2026-05-06 13:26:01 -0500

    address browser caching in head, reorder dropdown menu

--- src/OpenTenant_app/about/routes.py
+++ src/OpenTenant_app/about/routes.py
@@ -3,16 +3,14 @@ from flask import Blueprint, render_template
 about_bp = Blueprint('about', __name__, url_prefix='/about', template_folder='templates', static_folder='static', static_url_path='/about/static')
 
 
-@about_bp.route('/calendar')
-def calendar() -> None:
-    return render_template('about/calendar.html')
-
+@about_bp.route('/who_we_are')
+def who_we_are() -> None:
+    return render_template('about/who_we_are.html')
 
 @about_bp.route('/bylaws')
 def bylaws() -> None:
     return render_template('about/bylaws.html')
 
-
-@about_bp.route('/who_we_are')
-def who_we_are() -> None:
-    return render_template('about/who_we_are.html')
+@about_bp.route('/calendar')
+def calendar() -> None:
+    return render_template('about/calendar.html')
