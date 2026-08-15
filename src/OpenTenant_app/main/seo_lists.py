400ac2e2e44595d20b7ba503af2839a36db8ef49
upgrayedd <holden.rahn@gmail.com>
2026-08-15 12:52:45 -0500

    Adding robots.txt and sitemap.xml endpoints.

---
+++ src/OpenTenant_app/main/seo_lists.py
@@ -0,0 +1,15 @@
+ROBOTS_DISALLOW_LIST = [
+    '/account/',
+    '/admin/',
+    '/api/'
+]
+
+SITEMAP_ENDPOINTS = [
+    'about.bylaws',
+    'about.calendar',
+    'about.who_we_are',
+    'main.homepage',
+    'resources.know_before_renting',
+    'resources.legal_resources',
+    'resources.useful_links',
+]
